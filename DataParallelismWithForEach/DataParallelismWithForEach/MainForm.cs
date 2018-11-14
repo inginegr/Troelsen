20f503f12c286e366382f19ba867dc903eb3e579
Dmitry <inginegr@inbox.ru>
2018-11-14 18:44:31 +0600

    End offline mode goto WF DataTable

---
+++ DataParallelismWithForEach/DataParallelismWithForEach/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace DataParallelismWithForEach
+{
+    public partial class MainForm : Form
+    {
+        private CancellationTokenSource cts = new CancellationTokenSource();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+                {
+                    ProcessIntData();
+                });
+        }       
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+        }
+        private void ProcessIntData()
+        {
+            try
+            {
+                int[] innum = Enumerable.Range(1, 50000000).ToArray();
+                int[] mas3 = null;
+                try
+                {
+                    mas3 = (from nm in innum.AsParallel().WithCancellation(cts.Token) where nm % 3 == 0 orderby nm descending select nm).ToArray();
+                    MessageBox.Show(string.Format("Found {0} numbers that match query!", mas3.Count()));
+                }
+                catch (OperationCanceledException ex)
+                {
+                    this.Invoke((Action)delegate
+                    {
+                        textBox1.Text = ex.Message;
+                        cts.Dispose();
+                        cts = new CancellationTokenSource();
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("{0}", ex.Message));
+            }
+        }
+    }
+}
--- SharpTestConsole/SharpTestConsole/SimpleException.cs
+++ SharpTestConsole/SharpTestConsole/SimpleException.cs
@@ -5,6 +5,9 @@ using System.Data.Odbc;
 using System.Data.OleDb;
 using System.Configuration;
 using System.Data.Common;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using AutoLotConnectedLayer;
 
 namespace MyConnectionFactory
@@ -19,9 +22,14 @@ namespace MyConnectionFactory
             dts.ExtendedProperties["TimeStamp"] = DateTime.Now;
             dts.ExtendedProperties["DataSetID"] = Guid.NewGuid();
             dts.ExtendedProperties["Company"] = "Mikko's Hot Tub Super Store";
-
+            FillDataSet(dts);
+            PrintDataSet(dts);
+            SaveAndLoadXML(dts);
+            SaveAndLoadBinary(dts);
+            
             Console.ReadLine();
         }
+        #region
         static void FillDataSet(DataSet ds)
         {
             DataColumn dcCarID = new DataColumn("CarID", typeof(int));
@@ -32,12 +40,102 @@ namespace MyConnectionFactory
             dcCarID.AutoIncrement = true;
             dcCarID.AutoIncrementSeed = 0;
             dcCarID.AutoIncrementStep = 1;
+            
             DataColumn dcMake = new DataColumn("Make", typeof(string));
             DataColumn dcColor = new DataColumn("Color", typeof(string));
             DataColumn dcPetName = new DataColumn("PetName", typeof(string));
             dcPetName.Caption = "Pet Name";
             DataTable dt = new DataTable("Inventory");
             dt.Columns.AddRange(new DataColumn[] { dcCarID, dcMake, dcColor, dcPetName });
+            
+            DataRow carRow = dt.NewRow();
+            carRow["Make"] = "BMW";
+            carRow["Color"] = "Black";
+            carRow["PetName"] = "Hamlet";
+            dt.Rows.Add(carRow);
+            carRow = dt.NewRow();
+            carRow[1] = "Renault";
+            carRow[2] = "Duster";
+            carRow[3] = "Black";
+            dt.Rows.Add(carRow);
+            dt.PrimaryKey = new DataColumn[] { dt.Columns[0] };
+            ds.Tables.Add(dt);
+        }
+        private static void ManipulateDataRowState()
+        {
+            DataTable temp = new DataTable("Temp");
+            temp.Columns.Add(new DataColumn("TempColumn",typeof(int)));
+
+            DataRow row =temp.NewRow();
+            Console.WriteLine("After calling NewRow(): {0}",row.RowState);
+            temp.Rows.Add(row);
+            Console.WriteLine("After calling Rows.Add(): {0}", row.RowState);
+            row[0] = 10;
+            Console.WriteLine("After first assignment: {0}", row.RowState);
+
+            row.AcceptChanges();
+            Console.WriteLine("After calling AcceptChanges(): {0}", row.RowState);
+
+            row[0] = 11;
+            Console.WriteLine("After first assignment: {0}", row.RowState);
+
+            temp.Rows[0].Delete();
+            Console.WriteLine("After calling Delete: {0}", row.RowState);
+        }
+        static void PrintDataSet(DataSet ds)
+        {
+            Console.WriteLine("DataSet is named: {0}", ds.DataSetName);
+            foreach (System.Collections.DictionaryEntry de in ds.ExtendedProperties)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", de.Key, de.Value);
+            }
+            Console.WriteLine(ds.Tables.Count);
+            foreach (DataTable dd in ds.Tables)
+            {
+                Console.WriteLine("=> {0} Table:", dd.TableName);
+                for (int curCol = 0; curCol < dd.Columns.Count; curCol++)
+                {
+                    Console.Write(dd.Columns[curCol].ColumnName + "\t");
+                }
+                Console.WriteLine("\n------------------------------------------------");
+                PrintTable(dd);
+            }
+        }
+        static void PrintTable(DataTable dt)
+        {
+            DataTableReader dtr = dt.CreateDataReader();
+            while (dtr.Read())
+            {
+                for (int i = 0; i < dtr.FieldCount; i++)
+                {
+                    Console.Write("{0} \t", dtr.GetValue(i).ToString().Trim());
+                }
+            }
+            dtr.Close();
+        }
+        
+        static void SaveAndLoadXML(DataSet dts)
+        {
+            dts.WriteXml("docxml.xml");
+            dts.WriteXmlSchema("docxsd.xsd");
+
+            dts.Clear();
+            dts.ReadXml("docxml.xml");
+        }
+        #endregion
+
+        static void SaveAndLoadBinary(DataSet dts)
+        {
+            dts.RemotingFormat = SerializationFormat.Binary;
+            FileStream fs = new FileStream("BinaryCar.bin", FileMode.Create);
+            BinaryFormatter bfm = new BinaryFormatter(); 
+            bfm.Serialize(fs, dts);
+            fs.Close();
+
+            dts.Clear();
+
+            fs = new FileStream("BinaryCar.bin", FileMode.Open);
+            DataSet ds = (DataSet)bfm.Deserialize(fs);
         }
     }
 }
