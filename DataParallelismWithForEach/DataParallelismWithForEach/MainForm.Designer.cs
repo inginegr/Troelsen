c48b6a6cd762f93e1bb05054d7a5c19d2c46755e
Dmitry <inginegr@inbox.ru>
2018-11-14 19:04:39 +0600

    фы

--- DataParallelismWithForEach/DataParallelismWithForEach/MainForm.Designer.cs
+++ DataParallelismWithForEach/DataParallelismWithForEach/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.carInventoryGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.carInventoryGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,22 +42,35 @@
             this.carInventoryGridView.TabIndex = 0;
             this.carInventoryGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Here is what we stock";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(423, 358);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.carInventoryGridView);
             this.Name = "MainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.carInventoryGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView carInventoryGridView;
+        private System.Windows.Forms.Label label1;
 
     }
 }
--- DataParallelismWithForEach/DataParallelismWithForEach/MainForm.cs
+++ DataParallelismWithForEach/DataParallelismWithForEach/MainForm.cs
@@ -20,6 +20,16 @@ namespace DataParallelismWithForEach
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }        
     }
 }
