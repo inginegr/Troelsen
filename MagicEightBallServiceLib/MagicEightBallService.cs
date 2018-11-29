0a57594f9b835e95799b681fb9656247c8f335ed
Dmitry <inginegr@inbox.ru>
2018-11-29 16:41:29 +0600

    dll

---
+++ MagicEightBallServiceLib/MagicEightBallService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Xml;
+using System.Xml.Linq;
+using System.ServiceModel;
+
+
+namespace MagicEightBallServiceLib
+{
+    [ServiceContract(Namespace = "http://MyCompany.com")]
+    public interface IEightBall
+    {
+        [OperationContract]
+        string ObtainAnswerToQwestion(string userQuestion);
+    }
+
+    public class MagicEightBallService : IEightBall
+    {
+        public MagicEightBallService()
+        {
+            Console.WriteLine("The 8-ball awaits your question...");
+        }
+        public string ObtainAnswerToQwestion(string userQuestion)
+        {
+            string[] answers = { "Future Uncertain", "Yes", "No", "Hazy", "Ask again later", "Definitely" };
+            Random r = new Random();
+            return answers[r.Next(answers.Length)];
+        }
+    }
+}
