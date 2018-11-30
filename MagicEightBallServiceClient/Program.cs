107608cd15a41185344d352a210a938630362a87
Dmitry <inginegr@inbox.ru>
2018-11-30 16:49:26 +0600

    sm

---
+++ MagicEightBallServiceClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MagicEightBallServiceClient.ServiceReference1;
+
+
+namespace MagicEightBallServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Ask the Magic 8 Ball ***\n");
+            using (EightBallClient ball = new EightBallClient())
+            {
+                Console.WriteLine("Your question: ");
+                string question = Console.ReadLine();
+                string answer = ball.ObtainAnswerToQwestion(question);
+                Console.WriteLine("8-Ball says: {0}", answer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
