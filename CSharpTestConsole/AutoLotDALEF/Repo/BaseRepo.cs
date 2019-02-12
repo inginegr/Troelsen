6328543cc71fd949a7fa4c146e247919c5b9440a
abc <abc@example.com>
2019-02-12 23:48:33 +0600

    1

---
+++ CSharpTestConsole/AutoLotDALEF/Repo/BaseRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using AutoLotDALEF;
+
+namespace AutoLotDALEF.Repo
+{
+    public class BaseRepo<T> : IDisposable, IRepo<T> where T:EntityBase, new()
+    {
+        private readonly DbSet<T> table;
+        private readonly AutoLotEntities db;
+
+        public BaseRepo()
+        {
+            db = new AutoLotEntities();
+            table = db.Set<T>();
+        }
+
+        protected AutoLotEntities context => db;
+        public void Dispose()
+        {
+            db?.Dispose();
+        }
+    }
+}
