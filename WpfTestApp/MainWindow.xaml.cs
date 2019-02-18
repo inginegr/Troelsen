14161427668475b5f1c9fa4064702a07b4090c9f
Dmitry <inginegr@inbox.ru>
2019-02-18 12:13:32 +0600

    1

--- WpfTestApp/MainWindow.xaml.cs
+++ WpfTestApp/MainWindow.xaml.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Ink;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using Microsoft.Win32;
-
 
 
 namespace DataParallelismWithForEach
@@ -25,74 +14,34 @@ namespace DataParallelismWithForEach
     /// </summary>
     public partial class MainWindow : Window
     {
-        CancellationTokenSource cnt = new CancellationTokenSource();
-
+        
         public MainWindow()
         {
             InitializeComponent();
-        }        
-
-        private void MenuItem_MouseEnter_Exit(object sender, MouseEventArgs e)
-        {
-
+            this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
         }
 
-        private void MenuItem_MouseLeave_Exit(object sender, MouseEventArgs e)
-        {
-            
+        private void ColorChanged(object sender, SelectionChangedEventArgs e)
+        { 
+            string mcolor = (this.comboColors.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+            this.MyInkCanvas.DefaultDrawingAttributes.Color =  (Color)ColorConverter.ConvertFromString(mcolor);
         }
 
-        private void MenuItem_Click_Exit(object sender, RoutedEventArgs e)
+        private void RadioButtonClicked(object sender, RoutedEventArgs e)
         {
-            this.Close();
-        }
-
-        private void MenuItem_MouseEnter_Tools(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void MenuItem_MouseLeav_Tools(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void MenuItem_Click_Tools(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            int y = 8;
-
-            if (y > 7)
+            switch((sender as RadioButton)?.Name.ToString())
             {
-                e.CanExecute = true;
+                case "inkRadio":
+                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "eraseRadio":
+                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+                    break;
+                case "selectRadio":                    
+                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                    break;
             }
-            else
-                e.CanExecute = false;
-            
-        }
-
-        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            OpenFileDialog opf = new OpenFileDialog { Filter = "Text Files |*.txt" };
-            if (opf.ShowDialog() == true)
-            {
-                string str = File.ReadAllText(opf.FileName);
-            }
-        }
-
-        private void EllipseDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Hello from ellipse");
-            e.Handled = false;
-        }
-
-        private void Bla(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Hello");
         }
     }
 }
