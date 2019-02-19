46a767e94d22dd19742e516cd318e903fb463583
abc <abc@example.com>
2019-02-20 01:00:43 +0600

    1

--- WpfTestApp/MainWindow.xaml.cs
+++ WpfTestApp/MainWindow.xaml.cs
@@ -4,8 +4,26 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Ink;
-using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System;
+using System.Collections.Generic;
+using System.Collections;
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
 
 
 
@@ -16,50 +34,57 @@ namespace DataParallelismWithForEach
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        List<BitmapImage> img = new List<BitmapImage>();
+        int imgInd = 0;
+
         public MainWindow()
         {
             InitializeComponent();
-            this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
-            MyConv cnv = new MyConv();            
         }
 
-        private void ColorChanged(object sender, SelectionChangedEventArgs e)
-        { 
-            string mcolor = (this.comboColors.SelectedItem as ComboBoxItem)?.Content.ToString();
-            
-
-           // this.MyInkCanvas.DefaultDrawingAttributes.Color =  (Color)ColorConverter.ConvertFromString(mcolor);
-        }
-
-        private void RadioButtonClicked(object sender, RoutedEventArgs e)
+        private void BOne_Click(object sender, RoutedEventArgs e)
         {
-            switch((sender as RadioButton)?.Name.ToString())
+            //Next
+            if (imgInd >= img.Count)
+                imgInd = 0;
+            MyImg.Source = img[imgInd++];
+        }
+
+        private void BTwo_Click(object sender, RoutedEventArgs e)
+        {
+            //Prev
+            if (imgInd < 0)
+                imgInd = img.Count - 1;
+            MyImg.Source = img[imgInd--];
+        }
+
+        private void BThird_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
             {
-                case "inkRadio":
-                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
-                    break;
-                case "eraseRadio":
-                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
-                    break;
-                case "selectRadio":                    
-                    this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Select;
-                    break;
+                string path = Environment.CurrentDirectory;
+                img.Add(new BitmapImage(new Uri($@"{path}\Images\Chrysanthemum.jpg")));
+                img.Add(new BitmapImage(new Uri($@"{path}\Images\Desert.jpg")));
+                img.Add(new BitmapImage(new Uri($@"{path}\Images\Hydrangeas.jpg")));
+                img.Add(new BitmapImage(new Uri($@"{path}\Images\Jellyfish.jpg")));
+                img.Add(new BitmapImage(new Uri($@"{path}\Images\Koala.jpg")));
+                img.Add(new BitmapImage(new Uri($@"{path}\Images\Lighthouse.jpg")));
+                img.Add(new BitmapImage(new Uri($@"{path}\Images\Penguins.jpg")));
+                img.Add(new BitmapImage(new Uri($@"{path}\Images\Tulips.jpg")));
+
+                MyImg.Source = img[imgInd];
+
             }
-        }
-    }
+            catch (Exception)
+            {
 
-    public class MyConv : IValueConverter
-    {
-        public object Convert(object value, Type targettype, object param, System.Globalization.CultureInfo ci)
-        {
-            double v = (double)value;
-            return (int)v;
-        }
-
-        public object ConvertBack(object value, Type targettype, object param, System.Globalization.CultureInfo ci)
-        {
-            return value;
+                throw;
+            }
         }
     }
 }
