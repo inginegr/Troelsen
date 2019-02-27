23795035e31ad0beede45fae5ea0bc91dffd7854
Dmitry <inginegr@inbox.ru>
2019-02-27 16:53:39 +0600

    Work with Xaml

--- SharpTestConsole/SharpTestConsole/SimpleException.cs
+++ SharpTestConsole/SharpTestConsole/SimpleException.cs
@@ -2,46 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Data;
-using System.Xml;
-using System.Xml.Linq;
-using System.ServiceModel;
-using System.Collections;
-using System.Reflection;
-using System.Diagnostics;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Runtime.Remoting.Messaging;
-using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 using System.IO;
-using System.Data.Common;
-using System.Data.SqlClient;
 
 
-
-[assembly:CLSCompliant(true)]
 
 namespace MagicEightBallServiceHost
 {
-    
+    [Serializable]
+    [XmlType]
+    public class myc
+    {        
+        public string s1 { get; set; }
+        public string s2 { get; set; }
+        public string s3 { get; set; }        
+
+        public myc()
+        {
+            s1 = "Hello";
+            s2 = "my";
+            s3 = "Frends";
+        }
+    }
+
+    public class mys : myc
+    {
+        public string a1 { get; set; }
+        public string a2 { get; set; }
+        public string a3 { get; set; }
+
+        public mys()
+        {
+            a1 = "You";
+            a2 = "are";
+            a3 = "best";
+        }
+    }
     class Program
     {
-        private static string theEbook = null;
-        enum me
-        {
-            one, two, three
-        }
+        
         static void Main(string[] args)
         {
-            DirectoryInfo di = new DirectoryInfo(".");
-            FileInfo[] s = di.GetFiles();
-            foreach(FileInfo f in s)
-                Console.WriteLine(f.Name);
-            object sd;
-            Enum.IsDefined(typeof(me), "dsfsdf");
+            myc ms = new mys();
+            ms.s1 = "s11";
+            ms.s2 = "s2";
+            ms.s3 = "s3";
+
+            
+
+            try
+            {
+                XmlSerializer xser = new XmlSerializer(typeof(mys));
+
+                Stream fstr = new FileStream("mt.xml", FileMode.Create, FileAccess.Write, FileShare.None);
+                xser.Serialize(fstr, ms);
+                fstr.Close();
+
+                fstr = new FileStream("mt.xml", FileMode.Open);
+
+                myc vm = new myc();
+                Console.WriteLine($"{vm.s1}  {vm.s2}   {vm.s3}");
+                vm = (myc)xser.Deserialize(fstr);
+                Console.WriteLine($"{vm.s1}  {vm.s2}   {vm.s3}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
 
-            Console.ReadLine();
+    Console.ReadLine();
         }
     }
 }
