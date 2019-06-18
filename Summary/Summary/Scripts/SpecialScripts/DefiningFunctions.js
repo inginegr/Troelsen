aa169a8bf687f1809d908fef73d7d5c377f4d8d9
Dima@DimaPC <41135237+inginegr@users.noreply.github.com>
2019-06-18 23:33:43 +0600

    1

---
+++ Summary/Summary/Scripts/SpecialScripts/DefiningFunctions.js
@@ -0,0 +1,18 @@
+﻿
+window.onload = function (eventObj) {
+
+     //Вращение барабана
+
+
+//===========================================Элементы, вращающие барабан=======================================//
+    var skillsSwitchLeft = document.getElementById('skills').getElementsByClassName('switch left');
+    var skillsSwitchRight = document.getElementById("skills").getElementsByClassName('switch right');
+    var photoSwitchLeft = document.getElementById("photo").getElementsByClassName('switch left');
+    var photoSwitchRight = document.getElementById("photo").getElementsByClassName('switch right');
+
+    skillsSwitchLeft[0].onclick = function () {
+        alert("dfgdfgdfg");
+    }
+
+}
+
