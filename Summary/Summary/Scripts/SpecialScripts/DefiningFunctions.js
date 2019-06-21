3dbb870c1fc13269fdf433f11d63ac53814dc011
inginegr <41135237+inginegr@users.noreply.github.com>
2019-06-21 16:59:31 +0600

    1

--- Summary/Summary/Scripts/SpecialScripts/DefiningFunctions.js
+++ Summary/Summary/Scripts/SpecialScripts/DefiningFunctions.js
@@ -19,16 +19,38 @@ var procentFactors = {
 window.onload = function (eventObj) {
     // Количество циклов изменений
     var cycles = divis = 30;
+    // Коэффициент для поворота. 1 - влево, -1 - вправо
+    var i = 1;
 
-    // Теущие состояния
-    function currentStates(procentFactors, widthParam, heightParam) {
-        this.leftFor = widthParam * (1 - procentFactors.widthForeground) / 2; // Относительные величины, в долях, занимаемые панелями в родительском окне
-        this.kfLeftFor = procentFactors.alpha / cycles;
-        this.centerFor = 1;
-        this.kfCenterFor = (this.centerFor - (this.leftFor / (widthParam * procentFactors.widthForeground))) / cycles;
-        this.rightFor = widthParam * (1 - procentFactors.widthForeground) / 2;
-        this.alphaCenterFor = 0;
-        this.kfAlphaCenterFor = procentFactors.alpha / ( cycles);
+    // Флаг, показывающий, что в данный момент происходит вращение
+    var flagRotate = false;
+
+    // Теущие состояния, управляющие вращением
+    function currentStates(procentFactors, widthParam, heightParam, panels, directionParam) {
+        //-----------------------------------------Общие свойства--------------------------------------------------------//
+        alphaForeground = Math.atan((heightParam - panels[4].clientHeight) / (2 * panels[4].clientWidth));
+
+        //============================================Left foreground panel====================================================================//
+        this.leftForScale = 1;                                            // Относительные величины, в долях, занимаемые панелями в родительском окне
+        this.kfLeftForScale = (directionParam == "left") ? (1 / divis) : (panels[4].clientWidth / (divis * panels[5].clientWidth));
+        this.leftForScew = alphaForeground;
+        this.kfLeftForSkew = (directionParam == "left") ? (Math.PI / 2 - alphaForeground) : (alphaForeground / divis);
+        this.leftForTranslateX = 0;
+        this.kfLeftForTranslateX = (directionParam == "left") ? (panels[5] / divis) : (panels[5].clientWidth / divis);
+        this.leftForTranslateY = 0;
+        this.kfLeftForTranslateY = 
+
+        //============================================Center foreground panel====================================================================//
+        this.centerForScale = 1;
+        this.kfCenterForScale = (this.centerForScale - (panels[5].clientWidth / (widthParam * procentFactors.widthForeground))) / cycles;
+        this.centerForSkew = 0;
+        this.kfCenterForSkew = alphaForeground / divis;
+        this.centerForTranslateX = 0;
+        this.kfCenterForTranslateX = panels[4].clientWidth / divis;
+        //this.centerForTranslateY = 0;
+        //this.kfCenterForTranslateY = (heightParam - panels[4].clientHeight) / (2 * divis);
+        //=======================================================================================================================================//
+         
         // Доделать
         this.kfRightFor = 0;
         
@@ -48,21 +70,28 @@ window.onload = function (eventObj) {
     // Вращение блоков
     var rotateBlocks = function (directionRotate) {
 
-        try {
-            // Производим трансформации с панелями
+        try {          
             if (directionRotate == "right") {
-                curSt.centerFor -= curSt.kfCenterFor;
-                curSt.alphaCenterFor -= curSt.kfAlphaCenterFor
-                //alert(curSt.alphaCenterFor + " " + curSt.kfAlphaCenterFor);
-                panels[4].style.transformOrigin = "bottom left";
-                panels[4].style.transform = "scaleX(" + curSt.centerFor + ")" + " skewY(" + curSt.alphaCenterFor + "rad)";
-                //alert("scaleX(" + curSt.centerFor + ")");
+                i = -1;
+                panels[4].style.transformOrigin = "left bottom";
+            } else {
+                i = 1;
+                panels[4].style.transformOrigin = "right bottom";
             }
-            
+
+            // Работаем с центральной панелью переднего плана
+            curSt.centerForScale -= curSt.kfCenterForScale;
+            curSt.centerForSkew += i * curSt.kfCenterForSkew;
+            curSt.centerForTranslateX -=  i * curSt.kfCenterForTranslateX;            
+            panels[4].style.transform = "matrix(" + curSt.centerForScale + "," + curSt.centerForSkew + ", 0, 1," + curSt.centerForTranslateX + ", 0)";
+
+            // Левая панель переднего плана
+
             cycles--;
 
             if (cycles < 1) {
                 clearInterval(timeRotate);
+                flagRotate = false;
             }
         } catch (e) {
 
@@ -72,12 +101,19 @@ window.onload = function (eventObj) {
     }
     
     // Переключение влево-вправо
-    var doSwitch = function (eventObj) {        
+    var doSwitch = function (eventObj) {
+        // Проверяем, запущено ли измерение в текущий момент
+        if (flagRotate)
+            return;
+        else
+            flagRotate = true;
+        // Установить счетчик
+        cycles = divis;
         // Направление поворота
         var turnSide = "";
         // Блок, содержищий вращяющиеся элементы
         var blockRoot = null;
-                
+        // Родительский блок
         var trg = eventObj.target.parentNode;
 
         // Определяем направление поворота
@@ -88,16 +124,15 @@ window.onload = function (eventObj) {
 
         // Определяем блок, в котором происходит вращение
         blockRoot = trg.parentNode.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling;
-        
-        curSt = new currentStates(procentFactors, blockRoot.clientWidth, blockRoot.clientHeight);
-        
+                
         panels = blockRoot.querySelectorAll("div");
-        
+
+        curSt = new currentStates(procentFactors, blockRoot.clientWidth, blockRoot.clientHeight, panels, turnSide);
+
         // Вращаем блоки
         timeRotate = setInterval(function () {
             rotateBlocks(turnSide);
-        }, 10);
-        
+        }, 100);
     }
 
     //===========================================Элементы, вращающие барабан=======================================//
