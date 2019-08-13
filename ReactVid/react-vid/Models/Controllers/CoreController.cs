a35ac7b4dc28452fe7d62ede009158a1391fb3dd
inginegr <41135237+inginegr@users.noreply.github.com>
2019-08-13 16:58:16 +0600

    1

---
+++ ReactVid/react-vid/Models/Controllers/CoreController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace react_vid
+{
+    public class CoreController : Controller
+    {
+        public ViewResult ContCore()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ ReactVid/react-vid/Models/SomeCont.cs
@@ -0,0 +1,11 @@
+namespace react_vid
+{
+    public class SomeCont
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quty { get; set; }
+    }
+}
