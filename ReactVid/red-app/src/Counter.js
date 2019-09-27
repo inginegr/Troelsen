7b618ab9ebb0749f502fa4bb1fd5af2f3a2e368f
inginegr <41135237+inginegr@users.noreply.github.com>
2019-09-27 08:28:34 +0600

    1

--- ReactVid/red-app/src/Counter.js
+++ ReactVid/red-app/src/Counter.js
@@ -9,7 +9,7 @@ let Counter = ({counter, inc, dec, rnd}) => {
       <h2 id="counter">
         {counter}
       </h2>
-      <button id="inc" onClick={inc}>inc</button>
+      <button id="inc" onClick={inc}>incыы</button>
       <button id="dec" onClick={dec}>dec</button>
       <button id="rnd" onClick={rnd}>rnd</button>
     </div>
