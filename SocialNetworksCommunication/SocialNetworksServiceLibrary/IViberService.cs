6a6ecc0ba543650d7db33a4513d93cce944d4675
Dmitry <41135237+inginegr@users.noreply.github.com>
2019-10-22 20:41:42 +0600

    1

--- SocialNetworksCommunication/SocialNetworksServiceLibrary/IViberService.cs
+++ SocialNetworksCommunication/SocialNetworksServiceLibrary/IViberService.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -7,6 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 using SocialNetworks.Viber.Objects;
+
 
 namespace SocialNetworksServiceLibrary
 {
