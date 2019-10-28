36f3a2ea9fcce41489e971ae7d0c00279ecd6608
Dmitry <41135237+inginegr@users.noreply.github.com>
2019-10-28 16:53:08 +0600

    1

--- SocialNetworksCommunication/SocialNetworksRESTServices/Controllers/ViberController.cs
+++ SocialNetworksCommunication/SocialNetworksRESTServices/Controllers/ViberController.cs
@@ -67,11 +67,13 @@ namespace SocialNetworksRESTServices.Controllers
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+
         }
     }
 }
